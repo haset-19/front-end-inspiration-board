6f2e597d58e96699fd999fa23864e6d382e315ab
Laurel S <ls60@protonmail.com>
2022-01-04 01:20:07 -0800

    Sets state to hold selected board, updates display

--- src/App.js
+++ src/App.js
@@ -8,17 +8,33 @@ import { useState, useEffect } from 'react'
 // we will make a GET request to get boards when component renders
 
 function App() {
-  const [Boards, setBoards] = useState([])
+  const [boardsElements, setBoardsElements] = useState([])
   const [errorMessage, setErrorMessage] = useState("")
+  const [selectedBoard, setSelectedBoard] = useState({title:"Default Board"})
+  const [allBoards, setAllBoards] = useState("")
+
+  // figure out what is happening here!
+
+  // const handleClick = value => () => console.log(value)
+  // {
+  //   // console.log(event.target)
+  // }
+  const handleClick = board => () => {
+    console.log(board)
+    setSelectedBoard(board)
+  }
 
   useEffect( () => {
     axios.get("http://localhost:5000/boards")
       .then((response) => {
         const boardsArr = response.data
+        setAllBoards(boardsArr)
         const boardElements = boardsArr.map((item) => {
-          return <li key={item.id}>{item.title}</li>
+          return <li onClick={handleClick(item)}key={item.id}>
+                {item.title}
+                </li>
         })
-        setBoards(boardElements)
+        setBoardsElements(boardElements)
       })
       .catch((error) => {
         setErrorMessage(<section>{error.response.data.message}</section>)
@@ -35,13 +51,13 @@ function App() {
         <div className="board-list">
           <h2>Boards</h2>
           <ol>
-            {Boards}
+            {boardsElements}
           </ol>
         </div>
 
         <div className="selected-board">
           <h2>Selected Board</h2>
-          <h6>Board Name - Owner</h6>
+          <h4>{selectedBoard.title}</h4>
         </div>
       </section>
 
