47e70c658b64a295ad42804eb9764efcf11c7444
Laurel S <ls60@protonmail.com>
2022-01-04 00:47:34 -0800

    Calls API and dynamically renders list of boards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 import BoardsList from './components/BoardsList'
 import CardsList from './components/CardsList.js'
+import axios from 'axios';
+import { useState, useEffect } from 'react'
+
+
+// we will make a GET request to get boards when component renders
 
 function App() {
+  const [Boards, setBoards] = useState([])
+  const [errorMessage, setErrorMessage] = useState("")
+
+  useEffect( () => {
+    axios.get("http://localhost:5000/boards")
+      .then((response) => {
+        const boardsArr = response.data
+        const boardElements = boardsArr.map((item) => {
+          return <li key={item.id}>{item.title}</li>
+        })
+        setBoards(boardElements)
+      })
+      .catch((error) => {
+        setErrorMessage(<section>{error.response.data.message}</section>)
+      })
+  },[])
+
+
+
   return (
     <div className="App">
       <h1>Inspiration Board</h1>
-      <BoardsList />
-      <CardsList />
+
+      <section>
+        <div className="board-list">
+          <h2>Boards</h2>
+          <ol>
+            {Boards}
+          </ol>
+        </div>
+
+        <div className="selected-board">
+          <h2>Selected Board</h2>
+          <h6>Board Name - Owner</h6>
+        </div>
+      </section>
+
+      <section>
+        <CardsList />
+      </section>
+      {errorMessage}
     </div>
   );
 }
